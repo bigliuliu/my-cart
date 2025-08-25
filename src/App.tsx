899d760b9d1b30d21fa8db6df90ffbd1799fbe1e
bigliuliu <1552000425@qq.com>
2025-08-25 17:34:53 +0800

    fix browserrouter then add logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { Login } from "./pages/login";
 import { Product } from "./pages/product";
@@ -8,12 +8,10 @@ function App() {
   return (
     <Provider store={store}>
         <Header />
-        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login />}></Route>
             <Route path="/product" element={<Product />}></Route>
           </Routes>
-        </BrowserRouter>
     </Provider>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,35 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Avatar, Badge } from "antd";
 import { CartDrawer } from "../cartDrawer";
+import { loginOut } from "../../store/actions/userActions";
+import { useNavigate } from "react-router-dom";
 import "./index.less";
 export const Header = () => {
   const userState = useSelector((state) => state.user);
   const cartState = useSelector((state) => state.cart);
   const [cartTotal, setCartTotal] = useState(0);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     setCartTotal(cartState.reduce((total, item) => total + item.count, 0));
   }, [cartState]);
-
+  useEffect(() => {
+    if (userState.userInfo === null) {
+      localStorage.clear();
+      navigate("/");
+    }
+  }, [userState.userInfo, navigate]);
   const [openDrawer, setOpenDrawer] = useState(false);
   return (
     <>
       {userState.userInfo ? (
         <div className="header-wrap">
-          <p>
+          <p
+            onClick={() => {
+              dispatch(loginOut());
+            }}
+          >
             <span>{userState.userInfo.name}</span>
             <span>{userState.userInfo.email}</span>
           </p>
